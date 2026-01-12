6b9d4a17bee3c09ebd91f6a16fba8a45c37c1d89
mariana <eumarianamota@gmail.com>
2026-01-12 10:20:09 -0300

    update blog.tsx component

--- app/_components/blog.tsx
+++ app/_components/blog.tsx
@@ -6,9 +6,9 @@ import post4 from "@/public/post4.jpg"
 
 export function Blog() {
     return (
-        <section className="overflow-hidden text-gray-700 bg-white flex justify-center items-center px-10 py-20 text-center">
+        <section id="blog" className="overflow-hidden text-gray-700 bg-white flex justify-center items-center px-10 py-20 text-center">
             <div>
-                <h2 className="text-4xl text-center md:text-5xl lg:text-6xl font-bold white pb-15" >Nosso blog</h2>
+                <h2 data-aos="zoom-in-right" className="text-4xl text-center md:text-5xl lg:text-6xl font-bold white pb-15" >Nosso blog</h2>
                 <div className="flex flex-wrap justify-center gap-10 lg:gap-30">
                     <div className="flex flex-col justify-center items-center gap-5 transition-transform hover:scale-105 hover:brightness-95 duration-300 ease-in-out">
                         <Image 
